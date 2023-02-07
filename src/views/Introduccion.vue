714eb32c0b6cd00eb767cfc8d143e46ab4a71a69
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-02-07 16:11:38 -0500

    update Videos

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -26,7 +26,7 @@
     .row.mt-4
           figure
             .video
-              iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/UvyzFOv0wgE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
             figcaption   VIDEO
 
 
